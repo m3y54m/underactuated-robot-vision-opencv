1094157d71a3e999b07579211fc8b0537e8ce336
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-10 00:41:52 +0330

    Converted Arduino 'ino' to PlatformIO project

--- arduino_motor_control/src/main.cpp
+++ arduino_motor_control/src/main.cpp
@@ -1,36 +1,40 @@
-/*  Two Link Robot Control
+/*  Underactuated Robot Manipulator Control
 
     This program receives a command packet by serial port
     and send appropriate command to L298 motor driver IC
 
     1395/02/29
+
+    Refactored 1400/07/17
 */
 
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(2, 3); // RX, TX
+#include <Arduino.h>
 
-int enA = 9;    // the PWM pin of Motor A
-int in1 = 8;    // the Direction pin 1 of Motor A
-int in2 = 7;    // the Direction pin 2 of Motor A
+int enA = 9; // the PWM pin of Motor A
+int in1 = 8; // the Direction pin 1 of Motor A
+int in2 = 7; // the Direction pin 2 of Motor A
 
-int enB = 10;    // the PWM pin of Motor B
-int in3 = 11;    // the Direction pin 1 of Motor B
-int in4 = 12;    // the Direction pin 2 of Motor B
+int enB = 10; // the PWM pin of Motor B
+int in3 = 11; // the Direction pin 1 of Motor B
+int in4 = 12; // the Direction pin 2 of Motor B
 
-signed int cmdSpeedA = 0;    // the command for desired PWM of Motor A (0~254) - 255 = 0xFF is reserved for packet first character
-signed int cmdSpeedB = 0;    // the command for desired PWM of Motor B (0~254) - 255 = 0xFF is reserved for packet first character
+signed int cmdSpeedA = 0; // the command for desired PWM of Motor A (0~254) - 255 = 0xFF is reserved for packet first character
+signed int cmdSpeedB = 0; // the command for desired PWM of Motor B (0~254) - 255 = 0xFF is reserved for packet first character
 
-unsigned char packet[3];    // packet frame is: {0xFF, cmdSpeedA, cmdSpeedB}
+unsigned char packet[3]; // packet frame is: {0xFF, cmdSpeedA, cmdSpeedB}
 unsigned char index = 0;
 bool packetReceived = false;
 
 void motorA(int motorSpeed)
 {
-  if (motorSpeed > 255) motorSpeed = 255;
-  if (motorSpeed < -255) motorSpeed = -255;
-  if (abs(motorSpeed) < 2) motorSpeed = 0;
+  if (motorSpeed > 255)
+    motorSpeed = 255;
+  if (motorSpeed < -255)
+    motorSpeed = -255;
+  if (abs(motorSpeed) < 2)
+    motorSpeed = 0;
 
-  if (motorSpeed > 0)   // CCW Direction
+  if (motorSpeed > 0) // CCW Direction
   {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
@@ -52,11 +56,14 @@ void motorA(int motorSpeed)
 
 void motorB(int motorSpeed)
 {
-  if (motorSpeed > 255) motorSpeed = 255;
-  if (motorSpeed < -255) motorSpeed = -255;
-  if (abs(motorSpeed) < 2) motorSpeed = 0;
+  if (motorSpeed > 255)
+    motorSpeed = 255;
+  if (motorSpeed < -255)
+    motorSpeed = -255;
+  if (abs(motorSpeed) < 2)
+    motorSpeed = 0;
 
-  if (motorSpeed > 0)   // CCW Direction
+  if (motorSpeed > 0) // CCW Direction
   {
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
@@ -76,9 +83,9 @@ void motorB(int motorSpeed)
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(57600);
-   mySerial.begin(57600);
 
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
@@ -90,12 +97,14 @@ void setup() {
 
   motorA(0);
   motorB(0);
-  mySerial.println("Start");
+  Serial.println("Start");
 }
 
-void loop() {
-  if (packetReceived) {
-    mySerial.println("OK");
+void loop()
+{
+  if (packetReceived)
+  {
+    Serial.println("OK");
     cmdSpeedA = map(cmdSpeedA, 0, 254, -255, 255); // cmdSpeedA is mapped between -255 ~ 255
     cmdSpeedB = map(cmdSpeedB, 0, 254, -255, 255); // cmdSpeedB is mapped between -255 ~ 255
     motorA(cmdSpeedA);
@@ -110,14 +119,17 @@ void loop() {
     time loop() runs, so using delay inside loop can delay
     response.  Multiple bytes of data may be available.
 */
-void serialEvent() {
-  while (Serial.available()) {
+void serialEvent()
+{
+  while (Serial.available())
+  {
     // get the new byte:
     unsigned char inChar = (unsigned char)Serial.read();
     packet[index] = inChar;
     index++;
-    
-    if (index == 3) {
+
+    if (index == 3)
+    {
       if (packet[0] == 0xFF)
       {
         index = 0;
@@ -126,7 +138,8 @@ void serialEvent() {
         packetReceived = true;
         break;
       }
-      else {
+      else
+      {
         packet[0] = packet[1];
         packet[1] = packet[2];
         index = 2;
@@ -134,4 +147,3 @@ void serialEvent() {
     }
   }
 }
-
